ee5b465e4b3ff233b3c84bb252dc8eb56ec408db
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-26 10:47:08 -0500

    CREATED basic api
    set up a very basic api to test if database connection works

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { getAllGames, addGame } from "./src/games.js";
+
+const app = express();
+app.use(cors());
+
+// To Interact with the Game Collection
+app.get('/games', getAllGames);
+app.post('/games', addGame);
+
+// To Interact with the Gamer Card
+
+
+// To Interact with the Gamer Library
+
+
+export const api = functions.https.onRequest(app);
---
+++ functions/src/games.js
@@ -0,0 +1,24 @@
+import { ObjectId } from "mongodb";
+import { dbConnect } from "./dbConnect.js";
+
+// GET: All games from the games collection
+export async function getAllGames(req, res) {
+    const db = dbConnect();
+    const collection = await db.collection("games").find({}).limit(10).toArray();
+    
+    console.table(collection);
+    res.send(collection);
+}
+
+//POST: A game to the games collection
+export async function addGame(req, res) {
+    const newGame = req.body
+
+    const db = dbConnect();
+    const collection = db.collection("games").insertOne(newGame)
+        .catch(err => {
+            res.status(500).send(err)
+            return
+        });
+        res.status(201).send({message: "New game added to collection"})
+};
