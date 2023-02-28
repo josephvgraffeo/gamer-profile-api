3741f72001382be1e281b2046248dede8e9c0b4e
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-28 13:46:13 -0500

    ADDED route to patch gamercard
    route to patch gamercard about and tags

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ import functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import { getAllGames, addGame } from "./src/games.js";
-import { getGamerCard } from "./src/gamerCard.js";
+import { getGamerCard, patchGamerCard } from "./src/gamerCard.js";
 
 const app = express();
 app.use(cors());
@@ -13,6 +13,7 @@ app.post('/games', addGame);
 
 // To Interact with the Gamer Card
 app.get('/gamerCard', getGamerCard);
+app.patch('/gamerCard/:username', patchGamerCard);
 
 // To Interact with the Gamer Library
 
--- functions/src/gamerCard.js
+++ functions/src/gamerCard.js
@@ -1,10 +1,23 @@
 import { ObjectId } from "mongodb";
 import { dbConnect } from "./dbConnect.js";
 
+// get gamer card
 export async function getGamerCard(req, res) {
     const db = dbConnect();
     const collection = await db.collection("userGamerCard").find({}).limit(10).toArray();
     
     console.table(collection);
     res.send(collection);
+}
+
+// update gamercard by username
+export async function patchGamerCard(req, res) {
+    const { username } = req.params;
+    const { about, gamertags } = req.body;
+    const db = dbConnect();
+    
+    await db.collection("userGamerCard")
+        .updateOne({ username: username }, { $set: {about: about, gamertags: gamertags } })
+        .then(() => getGamerCard(req, res))
+        .catch(err => res.status(500).send({ message: err.message }));
 }
