0ef12d4e950330cbfcdc12732dda6fdddeb016be
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-02-28 13:56:39 -0500

    UPDATED patch to include greeting

--- functions/src/gamerCard.js
+++ functions/src/gamerCard.js
@@ -13,11 +13,11 @@ export async function getGamerCard(req, res) {
 // update gamercard by username
 export async function patchGamerCard(req, res) {
     const { username } = req.params;
-    const { about, gamertags } = req.body;
+    const { greeting, about, gamertags } = req.body;
     const db = dbConnect();
     
     await db.collection("userGamerCard")
-        .updateOne({ username: username }, { $set: {about: about, gamertags: gamertags } })
+        .updateOne({ username: username }, { $set: {greeting: greeting, about: about, gamertags: gamertags } })
         .then(() => getGamerCard(req, res))
         .catch(err => res.status(500).send({ message: err.message }));
 }
